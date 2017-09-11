7f7295b803228039d5afa064a8ddca2fc83f4148
Saisai Yang <1178850242@qq.com>
2017-09-11 21:47:25 +0800

    pyhon编程：从入门到实战游戏案例代码。

---
+++ settings.py
@@ -0,0 +1,28 @@
+class Settings():
+    """一个存储《外星人入侵》的所有设置的类"""
+
+    def __init__(self):
+        """初始化游戏的设置"""
+
+        # 屏幕设置
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (255, 255, 255)
+
+        # 飞船的设置
+        self.ship_speed_factor = 2.5
+        self.ship_limit = 3
+
+        # 子弹设置
+        self.bullet_speed_factor = 10
+        self.bullet_width = 300
+        self.bullet_height = 15
+        self.bullet_color = 20, 100, 20
+        self.bullets_allowed = 15
+
+        # 外星人设置
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+
+        # fleet_direction为1表示向右移，为-1表示向左移
+        self.fleet_direction = 1
